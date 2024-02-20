5bc3a8e873801d0220ae9c0e9aca1506b11ee58a
Navid <navidghate@hotmail.com>
2024-02-20 13:20:55 +0100

    added db connection on program.cs and added appsetting for connecting to correct sqlite file

--- Program.cs
+++ Program.cs
@@ -1,7 +1,14 @@
+using BookApp.Data;
+using Microsoft.EntityFrameworkCore;
+
 var builder = WebApplication.CreateBuilder(args);
 
 // Add services to the container.
 builder.Services.AddControllersWithViews();
+
+// db connection 
+builder.Services.AddDbContext<ApplicationDbContext>(options => 
+    options.UseSqlite(builder.Configuration.GetConnectionString("DefaultConnection")));
 
 var app = builder.Build();
 
